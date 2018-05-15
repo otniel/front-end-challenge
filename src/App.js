fc2b675b6091f94523dfc9a837615217ef63ee3e
Otniel Aguilar <otnieel.aguilar@gmail.com>
2018-05-15 17:58:08 -0500

    Improving project structure and adding LESS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './Assets/Images/logo.svg';
+import './Styles/App.css';
 
 class App extends Component {
   render() {
